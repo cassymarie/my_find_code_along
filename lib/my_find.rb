5c80153bbbe29f46821888a605617f4182396925
cassymarie <cassy2205@gmail.com>
2020-10-21 12:54:11 -0600

    commit tree 6ecd96182dcb654c95c2f004409cd8a0b5a916c1

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
+array = (1..100).to_a
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+        if yield(collection[i])
+            return collection[i]
+        end
+        i += 1
+    end
 end
+
+my_find(array){|i| i%3 == 0 && i%5 == 0}
